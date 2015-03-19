7c51583130efa8dd8473da2674342e7612107cb2
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-03-19 16:29:46 +0000

    adds the dock method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,11 @@
 require_relative 'bike'
 
 class DockingStation
-  @empty = true
   def release_bike
     Bike.new
-    fail 'No Bikes Available' if empty?
   end
-  attr_reader :empty
-  alias_method :empty?, :empty
+
+  def dock bike
+    bike
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  it { is_expected.to respond_to :empty? }
-  it { expect(-> { subject.release_bike }).to raise_error 'No Bikes Available' }
+  it 'can dock a bike' do
+    expect(subject.dock :bike).to eq :bike
+  end
 end
