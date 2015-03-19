42f11f53560e42f780b3e9064a5829d679fd2c3f
Phoebe Hugh <phoebehugh@gmail.com>
2015-03-19 15:49:25 +0000

    Adds lambda to test error when no bikes are available

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,5 +3,5 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   it { is_expected.to respond_to :empty? }
-  it { expect(subject.release_bike).to raise_error 'No Bikes Available' }
+  it { expect(-> { subject.release_bike }).to raise_error 'No Bikes Available' }
 end
